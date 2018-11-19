7dc0045760138e112edc63329c4e734b2daa66c6
geffreyw <geffreyw@hotmail.com>
2018-11-19 11:03:11 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgBootstrapModule} from './sharedModules/ng-bootstrap.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginModule } from './login/login.module';
+import { ErrorModule } from './error/error.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgBootstrapModule
+    NgBootstrapModule,
+    LoginModule,
+    ErrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
