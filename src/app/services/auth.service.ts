85b3c422bebfb735037ec34cceb782837a51c79b
Rijst <klaus.daems@outlook.com>
2018-11-23 14:00:45 +0100

    Login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {AlertBox} from '../interfaces/alert-box';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Login} from '../interfaces/Login';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,27 @@ import {AlertBox} from '../interfaces/alert-box';
 export class AuthService {
 
   alertBox$: BehaviorSubject<AlertBox> = new BehaviorSubject(null);
+  USERS_URL = 'https://hidden-citadel-73667.herokuapp.com/login';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  // Message BS4 alert-box
+  setMessage(msg: string, color: string) {
+    this.alertBox$.next({
+      message: msg,
+      color: color
+    });
+  }
 
   clearMessage() {
     this.alertBox$.next(null);
+  }
+
+  getToken(email: string, password: string): Observable<Login> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post<Login>(this.USERS_URL, {email, password}, httpOptions) ;
   }
 }
